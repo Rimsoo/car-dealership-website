1c77d9f639e672d0a0e1a33078286fbbcbd50dfb
GregV3 <grzegorz.muszynski77@gmail.com>
2023-08-01 14:58:35 +0200

    background video replaced with a few changing images

--- frontend/src/components/Wallpaper.js
+++ frontend/src/components/Wallpaper.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Wallpaper.css';
 
 export default function Wallpaper() {
+    const [background, setBackground] = useState(1);
+    let newBackground = 0;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            newBackground++;
+            newBackground === 6 && (newBackground = 1); // There is no sixth image so here the loop starts
+            setBackground(newBackground);
+        }, 4000);
+        return () => clearInterval(interval);
+    }, []);
+
 
     return (
-        <video src={require('../assets/cars-video.mp4')} autoPlay muted loop preload='true' playsInline />
+        // <video src={require('../assets/cars-video.mp4')} autoPlay muted loop preload='true' playsInline />
+        <img id='wallpaper' src={require(`../assets/wallpaper${background}.jpg`)} alt="Auta używane premium" />
     )
 }
