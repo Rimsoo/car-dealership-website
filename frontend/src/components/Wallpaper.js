952d5baf40491110a810419ed5b029ef4b9aee7c
GregV3 <grzegorz.muszynski77@gmail.com>
2023-09-26 15:01:27 +0200

    Background animations fixed

--- frontend/src/components/Wallpaper.js
+++ frontend/src/components/Wallpaper.js
@@ -1,33 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../components/Wallpaper.css';
 
 export default function Wallpaper() {
-    const [background, setBackground] = useState(1);
-    let newBackground = 0;
-    // let styling = {
-    //     display: 'block',
-    //     backgroundImage: `url(${require('../assets/backgrounds/wallpaper' + background + '.webp')})`
-    // }
-    let styling = {
-        backgroundImage: `url(${require('../assets/backgrounds/wallpaper' + background + '.webp')})`
+    const [background1, setBackground1] = useState(1);
+    const [background2, setBackground2] = useState(2);
+    const [shouldUpdateBackground1, setShouldUpdateBackground1] = useState(true);
+
+    let styling1 = {
+        backgroundImage: `url(${require('../assets/backgrounds/wallpaper' + background1 + '.webp')})`
+    }
+
+    let styling2 = {
+        backgroundImage: `url(${require('../assets/backgrounds/wallpaper' + background2 + '.webp')})`
     }
 
     useEffect(() => {
         const interval = setInterval(() => {
-            newBackground++;
-            newBackground === 6 && (newBackground = 1); // There is no sixth image so here the loop does another turn
-            setBackground(newBackground);
+            console.log("conditional " + shouldUpdateBackground1);
+            if (shouldUpdateBackground1) {
+                setBackground1(prevValue => {
+                    if (prevValue <  4) {
+                        return prevValue + 2;
+                    } else {
+                        return prevValue%3;
+                    }
+                });
+            } else {
+                setBackground2(prevValue => {
+                    if (prevValue <  4) {
+                        return prevValue + 2;
+                    } else {
+                        return prevValue%3;
+                    }
+                });
+            }
+            setShouldUpdateBackground1(prevValue => !prevValue);
         }, 5000);
-        return () => clearInterval(interval);
-    }, []);
 
+        return () => clearInterval(interval);
+    }, [shouldUpdateBackground1]);
 
     return (
-        // <video src={require('../assets/cars-video.mp4')} autoPlay muted loop preload='true' playsInline />
-        <div
-            id='wallpaper' 
-            alt="Auta używane premium" 
-            style={styling}
-        />
+        <div id='wallpaper'>
+            {/* Background no. 1 */}
+            <div
+                // When background 1 is waiting for being updated - must be hidden then
+                className={shouldUpdateBackground1 ? 'wallpaper__img wallpaper__img-hidden' : 'wallpaper__img wallpaper__img-shown'} 
+                alt="Auta używane premium" 
+                style={styling1}
+            />
+            {/* Background no. 2 */}
+            <div
+                // When background 1 is waiting for being updated - background 2 is shown
+                className={shouldUpdateBackground1 ? 'wallpaper__img wallpaper__img-shown' : 'wallpaper__img wallpaper__img-hidden'}
+                alt="Auta używane premium" 
+                style={styling2}
+            />
+        </div>
     )
 }
