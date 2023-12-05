c8a1474f556ee968f5f860eb4d2022b1abd15752
GregV3 <grzegorz.muszynski77@gmail.com>
2023-12-05 20:45:15 +0100

    Wallpaper component converted to TS

--- frontend/src/components/Wallpaper.tsx
+++ frontend/src/components/Wallpaper.tsx
@@ -1,24 +1,31 @@
 import { useState, useEffect } from 'react';
 import '../components/Wallpaper.css';
+import wallpaper1 from '../assets/backgrounds/wallpaper1.webp';
+import wallpaper2 from '../assets/backgrounds/wallpaper2.webp';
+import wallpaper3 from '../assets/backgrounds/wallpaper3.webp';
+import wallpaper4 from '../assets/backgrounds/wallpaper4.webp';
+import wallpaper5 from '../assets/backgrounds/wallpaper5.webp';
+
+const backgrounds = [wallpaper1, wallpaper2, wallpaper3, wallpaper4, wallpaper5];
 
 export default function Wallpaper() {
-    const [background1, setBackground1] = useState(1);
-    const [background2, setBackground2] = useState(2);
+    const [background1, setBackground1] = useState(0);
+    const [background2, setBackground2] = useState(1);
     const [shouldUpdateBackground1, setShouldUpdateBackground1] = useState(true);
 
     let styling1 = {
-        backgroundImage: `url(${require('../assets/backgrounds/wallpaper' + background1 + '.webp')})`
+        backgroundImage: `url(${backgrounds[background1]})`
     }
-
+    
     let styling2 = {
-        backgroundImage: `url(${require('../assets/backgrounds/wallpaper' + background2 + '.webp')})`
+        backgroundImage: `url(${backgrounds[background2]})`
     }
 
     useEffect(() => {
         const interval = setInterval(() => {
             if (shouldUpdateBackground1) {
                 setBackground1(prevValue => {
-                    if (prevValue <  4) {
+                    if (prevValue <  3) {
                         return prevValue + 2;
                     } else {
                         return prevValue%3;
@@ -26,7 +33,7 @@ export default function Wallpaper() {
                 });
             } else {
                 setBackground2(prevValue => {
-                    if (prevValue <  4) {
+                    if (prevValue <  3) {
                         return prevValue + 2;
                     } else {
                         return prevValue%3;
@@ -37,6 +44,7 @@ export default function Wallpaper() {
         }, 5000);
 
         return () => clearInterval(interval);
+
     }, [shouldUpdateBackground1]);
 
     return (
@@ -45,14 +53,12 @@ export default function Wallpaper() {
             <div
                 // When background 1 is waiting for being updated - must be hidden then
                 className={shouldUpdateBackground1 ? 'wallpaper__img wallpaper__img-hidden' : 'wallpaper__img wallpaper__img-shown'} 
-                alt="Auta używane premium" 
                 style={styling1}
             />
             {/* Background no. 2 */}
             <div
                 // When background 1 is waiting for being updated - background 2 is shown
                 className={shouldUpdateBackground1 ? 'wallpaper__img wallpaper__img-shown' : 'wallpaper__img wallpaper__img-hidden'}
-                alt="Auta używane premium" 
                 style={styling2}
             />
         </div>
--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -2,4 +2,5 @@ declare module '*.JPG';
 declare module "*.jpg";
 declare module "*.png";
 declare module "*.gif";
+declare module "*.webp";
 declare module 'split-text-js'
