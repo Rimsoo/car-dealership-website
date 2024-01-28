8053936524ebd637f6786bddab38d7e115d5bd65
GregV3 <grzegorz.muszynski77@gmail.com>
2024-01-28 19:48:42 +0100

    typing for image formats completed

--- frontend/src/pages/CarPage.tsx
+++ frontend/src/pages/CarPage.tsx
@@ -7,9 +7,7 @@ import ReactMarkdown from 'react-markdown';
 
 const strapiURL = 'https://kokpit.alfamotors.pl/';
 
-// The one below should get more than 25 given by a default
-const apiURL = 'https://kokpit.alfamotors.pl/api/cars?sort=date&pagination[start]=12&pagination[limit]=100&populate=* '; 
-
+    // Local
 // const strapiURL = 'http://localhost:1337';
 // const apiURL = 'http://localhost:1337/api/cars?populate=*'; 
 
@@ -76,14 +74,19 @@ interface Image {
   }
 
 export default function CarPage() {
-    const { loading, error, data } = useFetch(apiURL);
     const { id } = useParams();
+    // IDs of cars are not like the order due to deleted records (e.g. first record from db has ID = 12). This why the program uses pagination
+    let pagination: number = Number(id) - 20; // TS doesn't accept substracting number from string
+    if (pagination < 0) pagination = 0;
+    const apiURL = 'https://kokpit.alfamotors.pl/api/cars?sort=date&pagination[start]=' + pagination + '&pagination[limit]=100&populate=* '; 
+    
+    const { loading, error, data } = useFetch(apiURL);
     let imagesURLs: string[] = []; // here will be stored URLs of images
     let idCar: number = Number(id); // turninig string into number
 
-    
+    // A function checking if ID in URL is same as currently checked car from db
     function isThatCar(fetchedCar: Car[]) {
-        // Checking if ID in URL is same as currently checked car from db
+        // console.log(fetchedCar[1].id, idCar, pagination)
         return fetchedCar[1].id === idCar;
     }
     
