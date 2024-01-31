2a156723de0ab63ab2705fb579573bb1a4598079
GregV3 <grzegorz.muszynski77@gmail.com>
2024-01-31 19:40:35 +0100

    Image added in Financing page

--- frontend/src/pages/Financing.tsx
+++ frontend/src/pages/Financing.tsx
@@ -1,21 +1,25 @@
-import './Pages.css';    
+import './Financing.css';
+import coins from '../assets/coins.webp';    
 
 export default function Financing() {
 
     return (
         <div id='Financing'>
             <h1>FINANSOWANIE</h1>
-            <ul>
-                <li>
-                    Jesteśmy w stanie zorganizować wszystkie formy finansowania na bardzo korzystnych warunkach.
-                </li>
-                <li>
-                    Produkty finansowe dla firm oraz osób prywatnych (leasing operacyjny, leasing finansowy, pożyczka leasingowa czy kredyt).
-                </li>
-                <li>
-                    Współpracujemy z największymi bankami oraz firmami leasingowymi, co gwarantuje szeroki wybór oferty oraz kompleksową pomoc.
-                </li>
-            </ul>
+            <div className="Financing__container">
+                <img src={coins} alt='Samochody używane na leasing' />
+                <ul>
+                    <li>
+                        Jesteśmy w stanie zorganizować wszystkie formy finansowania na bardzo korzystnych warunkach.
+                    </li>
+                    <li>
+                        Produkty finansowe dla firm oraz osób prywatnych (leasing operacyjny, leasing finansowy, pożyczka leasingowa czy kredyt).
+                    </li>
+                    <li>
+                        Współpracujemy z największymi bankami oraz firmami leasingowymi, co gwarantuje szeroki wybór oferty oraz kompleksową pomoc.
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
