0a13bf0b814f5e399637a16f9ff7ab4b3a2a83dd
GregV3 <grzegorz.muszynski77@gmail.com>
2024-02-03 13:54:02 +0100

    About page refactored

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,23 +1,21 @@
-import './Pages.css';    
-
 export default function About() {
-
     return (
-        <div id="About">
-            <h1>O nas</h1>
+        <div id="About" className="mx-auto w-[90vw] pb-[80px] font-bebasFont
+        lg:w-[1000px] lg:pb-[50px]">
+            <h1 className="py-3 mb-0 text-center text-4xl font-medium underline underline-offset-[8px]">O nas</h1>
 
-            <div className="About__box">
-                <h2>Historia</h2>
+            <div className="mb-[20px]">
+                <h2 className="text-[35px] font-medium underline underline-offset-[4px] mb-[4px]">Historia</h2>
                 <p>
-                    <span>ALFA MOTORS</span> to rodzinna firma specjalizująca się w sprzedaży luksusowych samochodów marek premium, której tradycja sięga już prawie trzydziestu lat.
+                    <span className="font-bold">ALFA MOTORS</span> to rodzinna firma specjalizująca się w sprzedaży luksusowych samochodów marek premium, której tradycja sięga już prawie trzydziestu lat.
                 </p>
                 <p>
                     Powstała z pasji do motoryzacji, gdzie doświadczenie i wiedza o samochodach została przekazana z pokolenia na pokolenie.
                 </p>
             </div>
 
-            <div className="About__box">
-                <h2>Cel</h2>
+            <div className="mb-[20px]">
+                <h2 className="text-[35px] font-medium underline underline-offset-[4px] mb-[4px]">Cel</h2>
                 <p>
                     Naszym głównym założeniem jest indywidualne, szczere i uczciwe podejście do każdego klienta, aby świadomie wybrał najlepszy dla siebie samochód.
                 </p>
@@ -26,25 +24,25 @@ export default function About() {
                 </p>
             </div>
 
-            <div className="About__box">
-                <h2>Podstawa działalności</h2>
+            <div className="mb-[20px]">
+                <h2 className="text-[35px] font-medium underline underline-offset-[4px] mb-[4px]">Podstawa działalności</h2>
                 <p>
                     Wszystkie samochody są naszą własnością. Decyzja o zakupie poprzedzona jest zawsze gruntownym sprawdzeniem pojazdu oraz spełnieniem ostrych kryteriów selekcyjnych.
                 </p>
                 <p>
-                    <span>Kupujemy jedynie takie egzemplarze, które mają w pełni udokumentowaną historię użytkowania i serwisowania, są bezwypadkowe oraz często posiadają fabryczną gwarancję.</span>
+                    <span className="font-bold">Kupujemy jedynie takie egzemplarze, które mają w pełni udokumentowaną historię użytkowania i serwisowania, są bezwypadkowe oraz często posiadają fabryczną gwarancję.</span>
                 </p>
                 <p>
-                    <span>Samochody wyłącznie krajowe, zakupione w polskich salonach autoryzowanych dealerów.</span>
+                    <span className="font-bold">Samochody wyłącznie krajowe, zakupione w polskich salonach autoryzowanych dealerów.</span>
                 </p>
             </div>
 
-            <div className="About__box">
-                <h2>Zmiana samochodu - kompleksowe podejście</h2>
+            <div className="mb-[20px]">
+                <h2 className="text-[35px] font-medium underline underline-offset-[4px] mb-[4px]">Zmiana samochodu - kompleksowe podejście</h2>
                 <p>
                     Podchodzimy kompleksowo do zagadnienia jakim jest zmiana samochodu przez potencjalnego Klienta:
                 </p>
-                <ul>
+                <ul className="list-disc pl-[25px] m-[3px]">
                     <li>zapewniamy doradztwo w wyborze marki i modelu, który będzie spełniał Państwa oczekiwania</li>
                     <li>wyszukujemy pojazdy na Państwa zlecenie według konkretnych wymagań</li>
                     <li>jeżeli posiadacie Państwo pojazd w bardzo dobrym stanie, możemy go odkupić lub wziąć w rozliczeniu za samochód z naszej oferty</li>
@@ -52,7 +50,7 @@ export default function About() {
                 </ul>
             </div>
             
-            <h3>Zapraszamy do kontaktu przez formularz, mailowo lub telefonicznie!</h3>
+            <h3 className="text-center text-[28px] font-medium my-[20px] mx-auto">Zapraszamy do kontaktu przez formularz, mailowo lub telefonicznie!</h3>
         </div>
-    )
+    );
 }
