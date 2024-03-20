b2517e57ae07b1c9b4363f57910d9c74a8babba8
GregV3 <grzegorz.muszynski77@gmail.com>
2024-03-20 20:18:57 +0100

    Fetching data from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,10 @@ import SiteHeader from "./components/SiteHeader";
 import Thanks from "./pages/Thanks";
 import Wallpaper from "./components/Wallpaper";
 
+// hooks
+import useFetch from './hooks/useFetch';
+import mergeSort from './hooks/mergeSort';
+
 // Styling
 import './tailwind.css';
 
@@ -25,7 +29,83 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+
+// The code lines below get more than 25 records given by a default, the Strapi's limit is 100 (can't be increased)
+const apiURL1: string = 'https://kokpit.alfamotors.pl/api/cars?sort=date&pagination[pageSize]=100&populate=*';
+const apiURL2: string = 'https://kokpit.alfamotors.pl/api/cars?sort=date&pagination[start]=100&pagination[limit]=100&populate=*'; 
+
+// TypeScript
+interface CarAttributes {
+  state: string;
+  title: string;
+  mileage: number;
+  year: number;
+  fuel: string;
+  power: number;
+  gallery: GalleryItem;
+}
+
+interface GalleryItem {
+  data: {
+      attributes: {
+          formats: {
+              large: {
+                  url: string;
+              };
+              medium: {
+                  url: string;
+              };
+              small: {
+                  url: string;
+              };
+              thumbnail: {
+                  url: string;
+              };
+          };
+      };
+      id: number;
+  }[];
+}
+
+interface CarData {
+  id: string;
+  attributes: CarAttributes;
+}
+
 function App() {
+  const fetchedObject1 = useFetch(apiURL1);
+  const fetchedObject2 = useFetch(apiURL2);
+  
+  // if (fetchedObject1.loading || fetchedObject2.loading) return <Loader/>
+  // if (fetchedObject1.error || fetchedObject2.error) return <p>Error!!!</p>
+  if (fetchedObject1.loading || fetchedObject2.loading) return;
+  if (fetchedObject1.error || fetchedObject2.error) return;
+  
+  // Merging both arrays
+  const combinedData = [...fetchedObject1.data, ...fetchedObject2.data];
+  
+  // Using merge sort for setting cars in the order basing on "owners_number" attribute. It's treated as "kolejność" atttribute in the user's panel
+  const sortedCars = mergeSort(combinedData);
+
+  // Extracting offered cars
+  const filteredCarsOffered = sortedCars.filter((car: CarData[]) => car[1].attributes.state !== 'sold');
+  
+  // Extracting sold cars 
+  const filteredCarsSold = sortedCars.filter((car: CarData[]) => car[1].attributes.state === 'sold');
+  
+  // Splitting Sold cars into smaller arrays each containing up to 48 elements
+  function splitIntoChunks(array: []) {
+    const result= []
+    for (let i = 0; i < array.length; i += 48) {
+      const chunk = array.slice(i, i + 48);
+      result.push(chunk);
+    }
+    return result;
+  }
+  const arrayOfArraysWithSoldCars = splitIntoChunks(filteredCarsSold);
+
+  console.log(typeof arrayOfArraysWithSoldCars);
+
   return (
     <Router>
       <ApolloProvider client={client}>
@@ -39,8 +119,10 @@ function App() {
             <Route path='/carpage/:id' element={<CarPage />} /> 
             <Route path='/contact' element={<Contact />} /> 
             <Route path='/financing' element={<Financing />} /> 
-            <Route path='/' element={<Offer />} /> 
-            <Route path='/sold' element={<Sold />} /> 
+            <Route path='/' element={<Offer />} />
+
+            <Route path={`/sold/${1}`} element={<Sold arrayToDisplay={arrayOfArraysWithSoldCars} />} />
+
             <Route path='/thanks' element={<Thanks />} /> 
           </Routes>
           <Footer />
--- frontend/src/pages/Sold.tsx
+++ frontend/src/pages/Sold.tsx
@@ -1,74 +1,20 @@
 import Car from '../components/Car';
 import Loader from '../components/Loader';
-import useFetch from '../hooks/useFetch';
-import mergeSort from '../hooks/mergeSort';
 
 const strapiURL: string = 'https://kokpit.alfamotors.pl';
-
-// The code lines below get more than 25 records given by a default, the Strapi's limit is 100 (can't be increased)
-const apiURL1: string = 'https://kokpit.alfamotors.pl/api/cars?sort=date&pagination[pageSize]=100&populate=*';
-const apiURL2: string = 'https://kokpit.alfamotors.pl/api/cars?sort=date&pagination[start]=100&pagination[limit]=100&populate=*'; 
-
 
     // Local:
 // const strapiURL = 'http://localhost:1337';
 // const apiURL = 'http://localhost:1337/api/cars?populate=*'; 
 
-interface CarAttributes {
-    state: string;
-    title: string;
-    mileage: number;
-    year: number;
-    fuel: string;
-    power: number;
-    gallery: GalleryItem;
-}
-
-interface GalleryItem {
-    data: {
-        attributes: {
-            formats: {
-                large: {
-                    url: string;
-                };
-                medium: {
-                    url: string;
-                };
-                small: {
-                    url: string;
-                };
-                thumbnail: {
-                    url: string;
-                };
-            };
-        };
-        id: number;
-    }[];
-}
-  
-interface CarData {
-    id: string;
-    attributes: CarAttributes;
-}
-
-export default function Sold() {
-    const fetchedObject1 = useFetch(apiURL1);
-    const fetchedObject2 = useFetch(apiURL2);
-    
-    if (fetchedObject1.loading || fetchedObject2.loading) return <Loader/>
-    if (fetchedObject1.error || fetchedObject2.error) return <p>Error!!!</p>
-
-    const combinedData = [...fetchedObject1.data, ...fetchedObject2.data];
-    const filteredData = combinedData.filter((car: CarData[]) => car[1].attributes.state === 'sold')
-
-    // Using merge sort for setting cars in the order basing on "owners_number" attribute. It's treated as "kolejność" atttribute in the user's panel
-    const sortedFilteredData = mergeSort(filteredData);
+export default function Sold(props: any) {
 
     return (
         <div id="Sold">
             <div className='cards-area'>
                 <h1 className='page-title pb-8'>POJAZDY, KTÓRE ZNALAZŁY JUŻ NOWEGO WŁAŚCICIELA</h1>
-
+                <div>{props.arrayToDisplay}</div>
+{/* 
                 {sortedFilteredData.map((car: CarData[], index: number) => (
                         <Car 
                             key={'Car no ' + index}
@@ -82,7 +28,7 @@ export default function Sold() {
                             imageSource={strapiURL + car[1].attributes.gallery.data[0].attributes.formats.small.url}
                         />
                     )
-                )}
+                )} */}
             </div>
         </div>
     )
